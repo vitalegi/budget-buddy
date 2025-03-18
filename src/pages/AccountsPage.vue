4374eb986401c96a16cca6edf41daa011704dcf8
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2025-03-18 20:10:15 +0100

    move settings's sections to dedicated pages

---
+++ src/pages/AccountsPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="row content-start justify-evenly">
+    <AccountsVerticalEditor class="full-width"></AccountsVerticalEditor>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import AccountsVerticalEditor from 'src/components/accounts/AccountsVerticalEditor.vue';
+
+defineOptions({
+  name: 'AccountsPage',
+});
+</script>
+
+<style scoped lang="scss">
+.main-content {
+  overflow: auto;
+}
+
+.q-tab-panels {
+  background-color: transparent;
+}
+</style>
---
+++ src/pages/BackupPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="row content-start justify-evenly">
+    <SettingsList class="full-width"></SettingsList>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import SettingsList from 'src/components/settings/SettingsList.vue';
+
+defineOptions({
+  name: 'BackupPage',
+});
+</script>
+
+<style scoped lang="scss">
+.main-content {
+  overflow: auto;
+}
+
+.q-tab-panels {
+  background-color: transparent;
+}
+</style>
